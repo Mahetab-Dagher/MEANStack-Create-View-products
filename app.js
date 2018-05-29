69eff199ccfbce0ffa7098ff52056e33b25aa6cc
Mahetab-Dagher <mahetab.dagher@gmail.com>
2018-05-29 10:56:02 +0200

    try

--- app.js
+++ app.js
@@ -37,8 +37,8 @@ app.use('/user', userRoutes);
 app.use('/', appRoutes);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+/* app.use(function(req, res, next) {
   res.render('index');
-});
+}); */
 
 module.exports = app;
