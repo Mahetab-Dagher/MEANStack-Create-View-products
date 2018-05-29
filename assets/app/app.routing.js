efa6485ef3bcde77c321eafe13c6dd44b202d031
Mahetab-Dagher <mahetab.dagher@gmail.com>
2018-05-29 13:54:51 +0200

    try deploy

--- assets/app/app.routing.js
+++
@@ -1,7 +0,0 @@
-import { RouterModule } from "@angular/router";
-import { UserComponent } from "./user/user.component";
-var APP_ROUTES = [
-    { path: '', redirectTo: '/auth', pathMatch: 'full' },
-    { path: 'auth', component: UserComponent, loadChildren: './user/user.module#UserModule' }
-];
-export var routing = RouterModule.forRoot(APP_ROUTES);
